8944278f38af67cd7218e6518e677869cdc7fade
MatheusCarvalho <matheusbatista327@gmail.com>
2024-08-21 02:20:34 -0300

    Quarto

--- src/scripts.js
+++ src/scripts.js
@@ -9,29 +9,39 @@ window.onscroll = function() {
     }
 };
 
-function adjustLayout() {
+function ajustaLayout() {
     const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-    
+    // const screenHeight = window.innerHeight;
     // alert(screenWidth);
     if(screenWidth <= 1200){
         document.getElementsByClassName("logo")[0].style.justifyContent = "center";
-        document.getElementsByClassName("menu")[0].style.display = "none";
-        document.getElementsByClassName("contato")[0].style.display = "none";
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("sociais").style.display = "none";
         document.getElementsByClassName("menu-hamburger")[0].style.display = "flex";
         document.getElementsByClassName("foto")[0].style.display = "flex";
         document.getElementsByClassName("footer-contato")[0].style.flexDirection = "column";
+        
+        var listItems = document.querySelectorAll('#navbar ul li');     // Seleciona todos os elementos <li> dentro de #navbar
+        listItems.forEach(function(li) {                                // Itera sobre cada elemento <li> e altera sua margem
+            li.style.margin = '0';                                 // Define a nova margem
+        });
     }
     else{
         document.getElementsByClassName("logo")[0].style.justifyContent = "end";
-        document.getElementsByClassName("menu")[0].style.display = "flex";
-        document.getElementsByClassName("contato")[0].style.display = "flex";
+        document.getElementById("menu").style.display = "flex";
+        document.getElementById("sociais").style.display = "flex";
         document.getElementsByClassName("menu-hamburger")[0].style.display = "none";
         document.getElementsByClassName("foto")[0].style.display = "none";
         document.getElementsByClassName("footer-contato")[0].style.flexDirection = "row";
     }
 }
 
+function mostraMenu(){
+    var menu = document.getElementById('menu-suspenso');
+    menu.classList.toggle('show'); // Adiciona ou remove a classe "show"
+}
+
+
 // Ajusta o layout ao carregar a página e ao redimensionar
-window.addEventListener('load', adjustLayout);
-window.addEventListener('resize', adjustLayout);
+window.addEventListener('load', ajustaLayout);
+window.addEventListener('resize', ajustaLayout);
